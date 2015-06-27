6e769598053d8f7b79ecd2506fbadf57cdb18ecd
Nishanth Vijayan <nishanththegr8@gmail.com>
2015-06-27 17:28:55 +0530

    Total style rewrite.Added vote count,time,comments count,domain.

--- chrome/popup.js
+++ chrome/popup.js
@@ -1,14 +1,51 @@
 function putdata(res)
 { 
-  
   // removes the present posts
-  $(".content > li").remove();
-  $("hr").remove();
-  
+  $("#content > li").remove();
+  $("#content > hr").remove();
+
   $.each(res,function(i,post){
-    $(".content").append('<li data='+'"'+post.url+'"'+'><h3>'+(i+1)+".  "+post.title+'<h3><br></li><hr>');
+
+    var node = document.createElement("li");
+
+    var nameText = document.createTextNode((i+1)+".  "+post.title);
+    var nameNode = document.createElement("h3");
+    nameNode.data = post.url;
+    nameNode.appendChild(nameText);
+    node.appendChild(nameNode);
+
+    if(post.type=='link'){
+      var domainText = document.createTextNode('('+post.domain+')');
+      var domainNode = document.createElement("span");
+      domainNode.className = "detail";
+      domainNode.appendChild(domainText);
+      node.appendChild(domainNode);
+      node.appendChild(document.createElement("br"));
+    }
+    node.appendChild(document.createElement("br"));
+
+    score = (post.points!=null) ? post.points + '▲ ' : '';
+    comments = (post.comments_count=='1') ? ' • '+post.comments_count + ' comment ' : ' • '+post.comments_count + ' comments ';
+    detailText=document.createTextNode(score +' '+comments);
+    
+    var scoreNode = document.createElement("h5");
+    scoreNode.appendChild(detailText);
+    scoreNode.data = "https://news.ycombinator.com/item?id="+post.id;
+    scoreNode.className = "detail";
+    node.appendChild(scoreNode);
+
+    var timeText = document.createTextNode(' | '+post.time_ago);
+    var timeNode = document.createElement("h5");
+    timeNode.className = "detail";
+    timeNode.appendChild(timeText);
+    node.appendChild(timeNode);
+    node.appendChild(document.createElement("br"));
+    
+    document.getElementById("content").appendChild(node);
+    document.getElementById("content").appendChild(document.createElement("hr"));
   });
 }
+
 
 function fetchdata(){
   
@@ -57,26 +94,31 @@ $(document).ready(function(){
     localStorage.scrollTop = document.body.scrollTop;
   });
 
-  $("body").on('click',"li", function(){
-       chrome.tabs.create({url: $(this).attr('data')});
-       return false;
-     });
-
+  $("body").on('click',"li > h3", function(){
+    chrome.tabs.create({url: this.data});
+    return false;
+  });
+  
+  $("body").on('click',"h5", function(){
+    chrome.tabs.create({url: this.data});
+    return false;
+  });
+  
   $("body").on('click',"a", function(){
-     chrome.tabs.create({url: $(this).attr('data')});
-     return false;
-   });
+    chrome.tabs.create({url: this.data});
+    return false;
+  });
 
+  $("body").on('click',".gh-btn", function(){
+    chrome.tabs.create({url: "https://github.com/nishanthvijayan/HackerNews-Feed"});
+    return false;
+  });
   // this makes sure that fetchdata() is called only when the icon
   // is reload icon and not when it is the loading gif.
   $("body").on('click',".loading", function(){
     src = $('.loading').attr('src');
     if(src=="refresh-white.png") fetchdata();
   });
-
-	setTimeout(function(){
-    $("footer").prepend('&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span><iframe src="https://ghbtns.com/github-btn.html?user=nishanthvijayan&repo=hackernews-feed&type=star&count=true" frameborder="0" scrolling="0" width="100px" height="20px"></iframe></span>');
-  },1000);
 
 });
 
