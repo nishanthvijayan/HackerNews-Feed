1e1beafc47e9bc47d5939533fdd2dcf20be07c0a
Nishanth Vijayan <nishanththegr8@gmail.com>
2016-09-04 19:24:56 +0530

    Use single quotes everywhere

--- firefox/data/popup.js
+++ firefox/data/popup.js
@@ -1,26 +1,26 @@
 var clearPosts = function(){
-  $("#content > li").remove();
-  $("#content > hr").remove();
+  $('#content > li').remove();
+  $('#content > hr').remove();
 };
 
 var appendPostToList = function(post){
-  document.getElementById("content").appendChild(post);
-  document.getElementById("content").appendChild(document.createElement("hr"));
+  document.getElementById('content').appendChild(post);
+  document.getElementById('content').appendChild(document.createElement('hr'));
 };
 
 var renderTitle = function(index, post){
-  var titleNode = document.createElement("h3");
-  titleNode.data = (post.url.substring(0, 4) == "item") ? 'https://news.ycombinator.com/' + post.url : post.url;
+  var titleNode = document.createElement('h3');
+  titleNode.data = (post.url.substring(0, 4) == 'item') ? 'https://news.ycombinator.com/' + post.url : post.url;
 
-  var titleText = document.createTextNode((index + 1) + ".  " + post.title);
+  var titleText = document.createTextNode((index + 1) + '.  ' + post.title);
   titleNode.appendChild(titleText);
 
   return titleNode;
 };
 
 var renderDomainLink = function(post){
-  var domainLinkNode = document.createElement("span");
-  domainLinkNode.className = "detail";
+  var domainLinkNode = document.createElement('span');
+  domainLinkNode.className = 'detail';
 
   var domainLinkText = document.createTextNode('(' + post.domain + ')');
   domainLinkNode.appendChild(domainLinkText);
@@ -29,22 +29,22 @@ var renderDomainLink = function(post){
 }
 
 var renderDetails = function(post){
-    var detailsNode = document.createElement("h5");
+    var detailsNode = document.createElement('h5');
 
     var score = (post.points != null) ? post.points + '▲ ' : '';
     var comments = (post.comments_count == '1') ? ' • ' + post.comments_count + ' comment ' : ' • ' + post.comments_count + ' comments ';
     var detailsText = document.createTextNode(score + ' ' + comments);
 
     detailsNode.appendChild(detailsText);
-    detailsNode.data = "https://news.ycombinator.com/item?id=" + post.id;
-    detailsNode.className = "detail";
+    detailsNode.data = 'https://news.ycombinator.com/item?id=' + post.id;
+    detailsNode.className = 'detail';
 
     return detailsNode;
 };
 
 var renderTime = function(post){
-  var timeNode = document.createElement("span");
-  timeNode.className = "detail";
+  var timeNode = document.createElement('span');
+  timeNode.className = 'detail';
 
   var timeText = document.createTextNode(' | ' + post.time_ago);
   timeNode.appendChild(timeText);
@@ -58,21 +58,21 @@ function renderPosts(posts)
 
   $.each(posts, function(index, post){
 
-    var node = document.createElement("li");
+    var node = document.createElement('li');
     node.appendChild(renderTitle(index, post));
 
     if(post.type == 'link'){
       node.appendChild(renderDomainLink(post));
-      node.appendChild(document.createElement("br"));
+      node.appendChild(document.createElement('br'));
     }
 
-    node.appendChild(document.createElement("br"));
+    node.appendChild(document.createElement('br'));
 
     node.appendChild(renderDetails(post));
 
     node.appendChild(renderTime(post));
 
-    node.appendChild(document.createElement("br"));
+    node.appendChild(document.createElement('br'));
 
     appendPostToList(node);
   });
@@ -81,10 +81,10 @@ function renderPosts(posts)
 function fetchdata(){
   toggleLoadingAnimation();
   req =  new XMLHttpRequest();
-  req.open("GET", 'http://node-hnapi.herokuapp.com/news', true);
+  req.open('GET', 'http://node-hnapi.herokuapp.com/news', true);
   req.send();
   req.onload = function(){
-    self.port.emit("resizePanel");
+    self.port.emit('resizePanel');
     posts = JSON.parse(req.responseText);
     toggleLoadingAnimation();
     renderPosts(posts);
@@ -97,8 +97,8 @@ function fetchdata(){
 // toggles between the loading gif,reload icon.
 function toggleLoadingAnimation(){
   src = $('.loading').attr('src');
-  if(src=="img/refresh-white.png") $(".loading").attr("src", "img/ajax-loader.gif");
-  else $(".loading").attr("src", "img/refresh-white.png");
+  if(src=='img/refresh-white.png') $('.loading').attr('src', 'img/ajax-loader.gif');
+  else $('.loading').attr('src', 'img/refresh-white.png');
 }
 
 $(document).ready(function(){
@@ -106,33 +106,32 @@ $(document).ready(function(){
   fetchdata();
   setInterval(fetchdata, 300000);
 
-  //sends "link to be opened" to main.js
-  $("body").on('click', "li > h3", function(){
-    self.port.emit("postClicked", this.data);
+  $('body').on('click', 'li > h3', function(){
+    self.port.emit('postClicked', this.data);
     return false;
   });
 
-  $("body").on('click', "h5", function(){
-    self.port.emit("postClicked", this.data);
+  $('body').on('click', 'h5', function(){
+    self.port.emit('postClicked', this.data);
     return false;
   });
 
-  $("body").on('click', "a", function(){
-    self.port.emit("postClicked", $(this).attr('data'));
+  $('body').on('click', 'a', function(){
+    self.port.emit('postClicked', $(this).attr('data'));
     return false;
   });
 
-  $("body").on('click', ".gh-btn", function(){
-    self.port.emit("postClicked", "https://github.com/nishanthvijayan/HackerNews-Feed");
+  $('body').on('click', '.gh-btn', function(){
+    self.port.emit('postClicked', 'https://github.com/nishanthvijayan/HackerNews-Feed');
   });
 
-  $("body").on('click', ".loading", fetchdata);
+  $('body').on('click', '.loading', fetchdata);
 
-  $("body").on('click', ".up-btn", function(){
+  $('body').on('click', '.up-btn', function(){
     window.scrollTo(0, 0);
   });
 
   addEventListener('scroll', function(){
-    if($(".up-btn").css("display")=="none") $(".up-btn").show();
+    if($('.up-btn').css('display')=='none') $('.up-btn').show();
   });
 });
