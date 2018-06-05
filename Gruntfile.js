1bb9ca17f2e8365db2bfdb114b71d357ab03a6b1
Nishanth Vijayan <nishanthvijayan1995@gmail.com>
2018-06-05 18:46:51 +0530

    Clean build & temp folders first during build

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,10 +3,11 @@ module.exports = function(grunt) {
     // Project configuration.
     grunt.initConfig({
       pkg: grunt.file.readJSON('package.json'),
+      clean: ['build', 'clean'],
       browserify: {
           build: {
               src: "src/js/popup.js",
-              dest: "src/js/popup_bundle.js"
+              dest: "temp/popup_bundle.js"
           }
       },
       copy: {
@@ -21,8 +22,8 @@ module.exports = function(grunt) {
             { expand: true, cwd: 'src/', src: ['popup.html'], dest: 'build/chrome/' },
             { expand: true, cwd: 'src/', src: ['popup.html'], dest: 'build/firefox/'},
 
-            { expand: true, cwd: 'src/js', src: ['popup_bundle.js'], dest: 'build/chrome/js' },
-            { expand: true, cwd: 'src/js', src: ['popup_bundle.js'], dest: 'build/firefox/js'},
+            { expand: true, cwd: 'temp/', src: ['popup_bundle.js'], dest: 'build/chrome/js' },
+            { expand: true, cwd: 'temp/', src: ['popup_bundle.js'], dest: 'build/firefox/js'},
 
             { expand: true, cwd: 'src/css', src: ['chrome_style.css'], dest: 'build/chrome/css',
               rename: (dest, src) => { return dest + "/style.css" }
@@ -35,8 +36,9 @@ module.exports = function(grunt) {
       },
     });
 
+    grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-browserify');
 
-    grunt.registerTask('default', ['browserify','copy']);
+    grunt.registerTask('default', ['clean', 'browserify','copy']);
 }
