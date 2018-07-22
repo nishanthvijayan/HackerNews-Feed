30417f578e866ce6258bec44104c8c936228d35d
Nishanth Vijayan <nishanthvijayan1995@gmail.com>
2018-07-22 20:23:26 +0530

    Simplify webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,22 @@
-const CopyWebpackPlugin = require('copy-webpack-plugin')
-const CleanWebpackPlugin = require('clean-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
-module.exports = [
-	{
-    name: "chrome",
-    entry: {
-      "popup_bundle" : "./src/js/popup.js",
-
-    },
-    output:{
-      path: __dirname + '/build/chrome', 
-      filename: 'js/[name].js'
-    },
-    plugins: [
-      new CopyWebpackPlugin([
-        { from: 'src/css/chrome_style.css', to: 'css/style.css' },
-        { from: 'src/popup.html', to: 'popup.html' },
-        { from: 'src/manifest.json', to: 'manifest.json' },
-        { from: 'src/img', to: 'img' },
-      ]),
-      new CleanWebpackPlugin(['./build/chrome'])
-    ]
+module.exports = ['chrome', 'firefox'].map(platform => ({
+  name: platform,
+  entry: {
+    popup_bundle: './src/js/popup.js',
   },
-  {
-    name: "firefox",
-    entry: {
-      "popup_bundle" : "./src/js/popup.js",
-    },
-    output:{
-      path: __dirname + '/build/firefox', 
-      filename: 'js/[name].js'
-    },
-    plugins: [
-      new CopyWebpackPlugin([
-        { from: 'src/css/firefox_style.css', to: 'css/style.css' },
-        { from: 'src/popup.html', to: 'popup.html' },
-        { from: 'src/manifest.json', to: 'manifest.json' },
-        { from: 'src/img', to: 'img' },
-      ]),
-      new CleanWebpackPlugin(['./build/firefox'])
-    ]
-	}
-]
+  output: {
+    path: `${__dirname}/build/${platform}`,
+    filename: 'js/[name].js',
+  },
+  plugins: [
+    new CopyWebpackPlugin([
+      { from: `src/css/${platform}_style.css`, to: 'css/style.css' },
+      { from: 'src/popup.html', to: 'popup.html' },
+      { from: 'src/manifest.json', to: 'manifest.json' },
+      { from: 'src/img', to: 'img' },
+    ]),
+    new CleanWebpackPlugin([`./build/${platform}`]),
+  ],
+}));
